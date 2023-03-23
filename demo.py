b7489895c7afc4b3f2b7bf591c0bb8ae450a5ffb
BryanCa871 <bryansoe871@gmail.com>
2023-03-22 23:12:03 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,17 +3,20 @@ import json
 
 class mongodb2:
     def __init__(self):
+        pass
+
+    def mo(self, productos):
         try:
             cliente = MongoClient("mongodb+srv://admin:12345@cluster0.lksnws1.mongodb.net/?retryWrites=true&w=majority")
             db = cliente['DATOS']
             coleccion = db['DATOS']
-            for producto in self.productos.lista:
+            for producto in productos:
                 coleccion.insert_one(producto.__dict__)
             print("Productos agregados a MongoDB")
 
             # Actualizar cada documento con el valor de distancia correspondiente
             productos_coleccion = db['DATOS']
-            for producto in self.productos.lista:
+            for producto in productos:
                 codigo = producto.codigo
                 distancia = productos_coleccion.find_one({'codigo': codigo})['distancia']
                 producto.distancia = distancia
